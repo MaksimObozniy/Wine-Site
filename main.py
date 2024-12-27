fb0ab5e23de04a89749806756393d786ccf345df
MaksimObozniy <mobozn@gmail.com>
2024-12-27 14:56:38 +0400

    moving args and starting the server

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ def get_year_suffix(year):
         return "лет"
 
 
-def main(excel_path, excel_list_name):
+def main():
     
-    excel_data = pandas.read_excel(excel_path, 
-                                   sheet_name=excel_list_name, 
+    parser = argparse.ArgumentParser(description="Программа для изменение винной картотеки и применение акций исходя из данных таблиц Excel")
+    parser.add_argument("excel_file_path", type=str, help="Путь к файлу Excel где находится винная карта с ценами, названиями и тд.")
+    parser.add_argument("--excel_list_name", type=str, default="Лист1", help="Название страницы в фале Excel. По умолчанию 'Лист1'")
+    args = parser.parse_args()
+    
+    excel_data = pandas.read_excel(args.excel_file_path, 
+                                   sheet_name=args.excel_list_name, 
                                    usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'])
     
     excel_data = excel_data.where(pandas.notnull(excel_data), None)
-    excel_data_list = excel_data.to_dict(orient='records')
+    wine_data = excel_data.to_dict(orient='records')
 
     grouped_data = defaultdict(list)
-    for wine in excel_data_list:
+    for wine in wine_data:
         grouped_data[wine['Категория']].append(wine)
 
     env = Environment(  
@@ -43,21 +48,16 @@ def main(excel_path, excel_list_name):
 
     year_suffix = get_year_suffix(age_of_winery)
 
-    rendered_page = template.render(wines=excel_data_list, grouped_data=grouped_data, year_suffix=year_suffix, age_of_winery=age_of_winery)
+    rendered_page = template.render(wines=wine_data, grouped_data=grouped_data, year_suffix=year_suffix, age_of_winery=age_of_winery)
 
     with open('index.html', 'w', encoding="utf8") as f:
         f.write(rendered_page)
     
-    
-if __name__ == '__main__':
-    
-    parser = argparse.ArgumentParser(description="Программа для изменение винной картотеки и применение акций исходя из данных таблиц Excel")
-    parser.add_argument("excel_file_path", type=str, help="Путь к файлу Excel где находится винная карта с ценами, названиями и тд.")
-    parser.add_argument("--excel_list_name", type=str, default="Лист1", help="Название страницы в фале Excel. По умолчанию 'Лист1'")
-    args = parser.parse_args()
-    
-    main(args.excel_file_path, args.excel_list_name)
-    
     server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
     server.serve_forever()
+
+    
+if __name__ == '__main__':
+    main()
+    
 
